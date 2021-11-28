439e4effef18ca80976713297fbea4730e75c56d
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-11-28 15:55:08 +0300

    added task three

--- src/test/MainClassTest.java
+++ src/test/MainClassTest.java
@@ -7,11 +7,12 @@ import org.testng.Assert;
 public class MainClassTest {
 
     @Test
-    public void testGetClassNumber () {
+    public void testGetClassString () {
         MainClass mainClass = new MainClass ();
 
-        int number = mainClass.getClassNumber ();
-        Assert.assertTrue (number< 45,"Number error" );
+        String str = mainClass.getClassString ();
+        Assert.assertTrue ( str.contains ("Hello") || str.contains ("hello") , "Contains the string");
+
 
 
     }
