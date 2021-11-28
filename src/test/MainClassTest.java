a1d73b95217c68cac465520f6478632c05f0b57e
A.Elizaveta <Alimbekova.elizaveta98@mail.ru>
2021-11-28 15:25:01 +0300

    added task two

--- src/test/MainClassTest.java
+++ src/test/MainClassTest.java
@@ -1,24 +1,21 @@
 package test;
 
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 
 public class MainClassTest {
 
-    MainClass main = new MainClass ();
-
-
     @Test
-    void testGetLocalNumber () {
-        int number = this.main.getLocalNumber ();
-        if (number > 14) {
-            System.out.println ("The number cannot be less than 14");
-        }
-        if (number < 14) {
-            System.out.println ("The number cannot be more than 14");
-        } else {
-            System.out.println ("True");
+    public void testGetClassNumber () {
+        MainClass mainClass = new MainClass ();
 
-        }
+        int number = mainClass.getClassNumber ();
+        Assert.assertTrue (number< 45,"Number error" );
+
+
     }
+
+
 }
+
